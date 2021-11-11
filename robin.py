8456a3f3250b1bb67e156092fb96486c689da93e
PiciAkk <marci@dbx.hu>
2021-11-11 18:56:45 +0100

    big update and shebang added

--- robin.py
+++ robin.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python3
 import sys
 import pip
 import requests
@@ -10,26 +11,43 @@ class csomagkezelo:
                 pip.main(['install', csomagnev])
             else:
                 pip._internal.main(['install', csomagnev])
+    def fuggosegLetorles(self, fuggosegek):
+        for csomagnev in fuggosegek:
+            if hasattr(pip, 'main'):
+                pip.main(['uninstall', csomagnev])
+            else:
+                pip._internal.main(['uninstall', csomagnev])
     def telepites(self, csomagnev):
         baseURL = f"https://raw.githubusercontent.com/PiciAkk/ROBIN5-Csomagok/main/{csomagnev}"
         modulFajl = open(f"csomagok/{csomagnev}.py", "w+")
         modulFajl.write(requests.get(f"{baseURL}/modul.py").text)
         modulFajl.close()
         fuggosegek = (requests.get(f"{baseURL}/fuggosegek.txt").text).split()
-        print(fuggosegek)
         self.fuggosegTelepites(fuggosegek)
+        print("Csomag sikeresen telepítve")
     def letorles(self, csomagnev):
-        print("remove...")
+        fuggosegek = (requests.get(f"https://raw.githubusercontent.com/PiciAkk/ROBIN5-Csomagok/main/{csomagnev}/fuggosegek.txt").text).split()
+        try:
+            os.remove(f"csomagok/{csomagnev}.py")
+        except:
+            raise Exception("Csomag nincs telepítve!")
+        self.fuggosegLetorles(fuggosegek)
+        print("Csomag sikeresen törölve!")
     def parameterTeszt(self, args):
         if len(args) < 3:
             raise Exception("Csomagnév nincs specifikálva!")
         elif len(args) > 3:
             raise Exception("Túl sok paraméter")
 class ROBIN:
-    def __init__(self):
-        modulok = os.listdir('./csomagok')
+    def hangFelismeres(self):
+        return "Szia"
+    def csomagBetoltes(self):
+        modulok = os.listdir("./csomagok")
         for modul in modulok:
-            eval(open(f"csomagok/{modul}", "r").read())
+            exec(open(f"csomagok/{modul}", "r").read())
+    def __init__(self):
+        self.parancs = self.hangFelismeres()
+        self.csomagBetoltes()
 
 def main():
     csomagok = csomagkezelo()
